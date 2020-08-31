b3a8aa0ce7b0b8b581dfe3dab44329e3f8dc1873
David <djadell.e@gmail.com>
2020-08-31 19:18:38 +0200

    Create a SeachController and viewModel

--- GitHubInfo/Controllers/ViewController.swift
+++ GitHubInfo/Controllers/ViewController.swift
@@ -7,14 +7,39 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    let searchController = UISearchController(searchResultsController: nil)
+    var searchBar: UISearchBar { return searchController.searchBar }
+    
+    var viewModel = ViewModel()
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configureSearchController()
+        
+        viewModel.data.drive(tableView.rx.items(cellIdentifier: "Cell")) { _, Repository, cell in
+            cell.textLabel?.text = Repository.repoName
+            cell.detailTextLabel?.text = Repository.repoURL
+        }
+        .disposed(by: disposeBag)
+        
     }
-
-
+    
+    func configureSearchController() {
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchBar.showsCancelButton = true
+        searchBar.text = "NavdeepSinghh"
+        searchBar.placeholder = "Enter GitHub ID, e.g., \"NavdeepSinghh\""
+        tableView.tableHeaderView = searchController.searchBar
+        definesPresentationContext = true
+    }
 }
 
