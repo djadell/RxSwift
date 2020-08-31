ae8807a2602b29f8923c5fade5f86e2eed816728
David <djadell.e@gmail.com>
2020-08-31 17:18:01 +0200

    create project

---
+++ GitHubInfo/GitHubInfo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubInfo
+//
+//  Created by David Adell Echevarria on 31/08/2020.
+//  Copyright © 2020 djadell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
